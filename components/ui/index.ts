d912df37e593538cc72502b2d2a7f4c93a422c1d
laynexx <argo150808@gmail.com>
2024-10-26 14:29:48 +0300

    init + { titles, header}

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button} from './button';
+export { Checkbox} from './checkbox';
+export {Dialog} from './dialog';
+export {Input} from './input';
+export {Popover} from './popover';
+export {RadioGroup} from './radio-group';
+export {Select} from './select';
+export {Skeleton} from './skeleton';
+export {Slider} from './slider';
